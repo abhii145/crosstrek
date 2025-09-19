d6385a1e4c845d67026af8c6c31b771af7f47fde
Abhishek Yadav <55094244+abhii145@users.noreply.github.com>
2025-09-19 15:10:28 +0530

    Create index.js

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import About from "./About"
+import Classes from "./Classes"
+import ExpertTrainers from "./ExpertTrainers"
+import Gallery from "./Gallery"
+import Home from "./Home"
+import Navbar from "./Navbar"
+import Reviews from "./Reviews"
+import Footer from "./Footer"
+
+export {
+  About,
+  Classes,
+  ExpertTrainers,
+  Gallery,
+  Home,
+  Navbar,
+  Reviews,
+  Footer,
+}
